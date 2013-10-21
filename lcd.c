28912172c8a4d2ba374aa0f972ef6fd80542c985
Ryan Hub <c15ryan.hub@usafa.edu>
2013-10-20 21:37:29 -0600

    started the header file by initializng the different subroutines used

---
+++ lcd.c
@@ -0,0 +1,10 @@
+/*
+ * lcd.c
+ *
+ *  Created on: Oct 20, 2013
+ *      Author: c15ryan.hub
+ */
+
+
+
+
---
+++ lcd.h
@@ -0,0 +1,14 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Oct 20, 2013
+ *      Author: c15ryan.hub
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+
+
+#endif /* LCD_H_ */
