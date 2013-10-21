1d96e8221876562a0d1a532f66ccff20e3c5832a
Ryan Hub <c15ryan.hub@usafa.edu>
2013-10-20 21:51:08 -0600

    Here are the function prototypes

--- lcd.h
+++ lcd.h
@@ -8,7 +8,12 @@
 #ifndef LCD_H_
 #define LCD_H_
 
-
+void initSPI();
+void lcdClear();
+void lcdInitialize();
+void displayScreen(String Message);
+void scrollDisplay(String Message);
+void scrollMessage();
 
 
 #endif /* LCD_H_ */
